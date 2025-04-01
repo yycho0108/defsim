df5d7c9de485d20f621ab077237a11499d7a2a1a
okj001010 <okj001010@gmail.com>
2025-04-01 16:06:00 +0900

    just commit

--- scripts/oneCircle.py
+++ scripts/oneCircle.py
@@ -13,18 +13,18 @@ def main():
     sim = Simulation("test", res=(800,600), dt=0.01, iterations=3)
 
     # add cloth
-    # cloth = GridCloth(
-    #     N=(32,32),
-    #     cell_size=(0.5, 0.5),
-    #     center=(0.0,0.0),
-    #     gravity=-9.8,
-    #     dt=0.01,
-    #     wind=(0.0,0.0),
-    #     color=(0.5,0.5,0.5),
-    #     stiffness=1.0,
-    #     damping=0.99
-    # )
-    # sim.add_cloth(cloth)
+    cloth = GridCloth(
+        N=(32,32),
+        cell_size=(0.5, 0.5),
+        center=(0.0,0.0),
+        gravity=-9.8,
+        dt=0.01,
+        wind=(0.0,0.0),
+        color=(0.5,0.5,0.5),
+        stiffness=1.0,
+        damping=0.99
+    )
+    sim.add_cloth(cloth)
 
     # add circle
     circle = Circle(center=(0.0, 0.3), radius=0.1, color=(1,0,0), drest=0.005)
